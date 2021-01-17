efefa420b462e4a3929cd6dbf843462ac77a8d1c
moomu-owo <71679979+moomu-owo@users.noreply.github.com>
2021-01-17 21:43:14 +0800

    加入了陶可的動畫了 喔

---
+++ 3D_End_107051046/Assets/scripts/Enemy.cs
@@ -0,0 +1,130 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;   //引用 人工智慧 API
+
+
+
+public class Enemy : MonoBehaviour
+{
+
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 3;
+    [Header("停止距離"), Range(0, 50)]
+    public float stopDistance = 2.5f;
+    [Header("攻擊冷卻時間"), Range(0, 50)]
+    private float cd = 2f;
+    [Header("攻擊中心點")]
+    public Transform atkPoint;
+    [Header("攻擊長度"), Range(0f, 5f)]
+    public float atkLength;
+    [Header("攻擊力"), Range(0, 500)]
+    public float atk = 30;
+
+
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator ani;
+    /// <summary>
+    /// 計時器
+    /// </summary>
+    private float timer;
+
+
+
+    private void Awake()
+    {
+        //取得身上的物件<代理器>
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+
+        //尋找其他遊戲物件("物件名稱").變形物件
+        player = GameObject.Find("ming").transform;
+
+        //代理器的 速度 與 停止距離
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+    }
+
+    private void Update()
+    {
+        Track();
+        Attack();
+    }
+
+
+
+    ///射線擊中的物件
+    private RaycastHit hit;
+
+    public float hp = 100;
+
+    ///攻擊
+    private void Attack()
+    {
+        if (nav.remainingDistance < stopDistance)
+        {
+            //時間 累加 (一幀的時間)
+            timer += Time.deltaTime;
+
+            //取得玩家的座標
+            Vector3 pos = player.position;
+            //將 玩家的座標Y軸 指定為 本物件的Y軸
+            pos.y = transform.position.y;
+            //看向(玩家的座標)
+            transform.LookAt(pos);
+
+            //如果 計時器 >= 冷卻時間 就攻擊  並且歸零計時器
+            if (timer >= cd)
+            {
+                ani.SetTrigger("attack");
+                timer = 0;
+
+                //物理.射線碰撞(攻擊中心點的座標，攻擊中心點的前方，射線擊中的物件，攻擊長度，圖層)
+                //圖層: 1<<圖層編號
+                //如果射線  擊中物件  就執行()
+                if (Physics.Raycast(atkPoint.position, atkPoint.forward, out hit, atkLength, 1 << 8))
+                {
+                    //碰撞物件.取得元件<玩家>().受傷()
+                    hit.collider.GetComponent<Player>().Damage(atk);
+                }
+            }
+        }
+    }
+
+    ///受傷
+    ///<param name="damage">接收的傷害值 </param>
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        ani.SetTrigger("damage");
+        if (hp <= 0) Dead();
+    }
+
+    private void Dead()
+    {
+        nav.isStopped = true;  //關閉 導覽器
+        enabled = false;       // 關閉 腳本
+        ani.SetBool("die", true);   // 死亡動畫
+    }
+
+    /// <summary>
+    /// 繪製圖示事件:僅在Unity內顯示
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        //圖示.顏色 = 紅色
+        Gizmos.color = Color.green;
+        //圖示.繪製射線(中心點，方向)
+        //(攻擊中心點座標，攻擊中心點的前方 *攻擊長度)
+        Gizmos.DrawRay(atkPoint.position, atkPoint.forward * atkLength);
+    }
+
+    ///追蹤
+    private void Track()
+    {
+        //代理器.設定目的地(玩家的位置)
+        nav.SetDestination(player.position);
+        //動畫控制器.設定布林值("參數名稱"，剩餘距離>停止距離)
+        ani.SetBool("run", nav.remainingDistance > stopDistance);
+    }
+}
---
+++ 3D_End_107051046/Assets/scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using Invector.vCharacterController;  //引用 套件
+
+public class Player : MonoBehaviour
+{
+    private float hp = 100;
+    private Animator ani;
+
+
+
+    ///連擊次數
+    private int atkCount;
+
+    ///計時器
+    private float timer;
+    [Header("連擊間隔時間"), Range(0, 3)]
+    public float interval = 1;
+    [Header("攻擊中心點")]
+    public Transform atkPoint;
+    [Header("攻擊長度"), Range(0f, 5f)]
+    public float atkLength;
+    [Header("攻擊力"), Range(0, 500)]
+    public float atk = 30;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    /// <summary>
+    /// 繪製圖示事件:僅在Unity內顯示
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        //圖示.顏色 = 紅色
+        Gizmos.color = Color.green;
+        //圖示.繪製射線(中心點，方向)
+        //(攻擊中心點座標，攻擊中心點的前方 *攻擊長度)
+        Gizmos.DrawRay(atkPoint.position, atkPoint.forward * atkLength);
+    }
+
+    ///射線擊中的物件
+    private RaycastHit hit;
+
+    private void Attack()
+    {
+        if (atkCount < 3)
+        {
+            if (timer < interval)
+            {
+                timer += Time.deltaTime;
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    atkCount++;
+                    timer = 0;
+                    if (Physics.Raycast(atkPoint.position, atkPoint.forward, out hit, atkLength, 1 << 9))
+                    {
+                        hit.collider.GetComponent<Enemy>().Damage(atk);
+                    }
+                }
+            }
+            else
+            {
+                atkCount = 0;
+                timer = 0;
+            }
+        }
+        if (atkCount == 3) atkCount = 0;
+        ani.SetInteger("連技", atkCount);
+    }
+
+    ///受傷
+    ///<param name="damage">接收的傷害值 </param>
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        ani.SetTrigger("damage");
+        if (hp <= 0) Dead();
+    }
+
+    private void Dead()
+    {
+        ani.SetTrigger("die");
+
+        //鎖定移動與旋轉
+        vThirdPersonController vt = GetComponent<vThirdPersonController>();
+        vt.lockMovement = true;
+        vt.lockRotation = true;
+    }
+}
